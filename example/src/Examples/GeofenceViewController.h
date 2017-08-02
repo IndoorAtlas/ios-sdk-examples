bc8f85ef965b15f9d8a72ffc75e5f52188efbe31
Jari Vetoniemi <jari.vetoniemi@indooratlas.com>
2017-08-02 12:03:34 +0300

    Base geofence on map overlay example
    Geofence can be added to current location by tapping the screen.

--- example/src/Examples/GeofenceViewController.h
+++ example/src/Examples/GeofenceViewController.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
-@interface GeofenceViewController : UIViewController
+#import "AppleMapsOverlayViewController.h"
+
+@interface GeofenceViewController : AppleMapsOverlayViewController
 
 @end
--- example/src/Examples/GeofenceViewController.m
+++ example/src/Examples/GeofenceViewController.m
@@ -6,136 +6,122 @@
 #import "GeofenceViewController.h"
 #import "../ApiKeys.h"
 
-@interface GeofenceViewController () <IALocationManagerDelegate>
-@property (nonatomic, strong) IALocationManager *manager;
-@property (nonatomic, strong) UILabel *regionLabel;
-@property (nonatomic, strong) NSString *geofenceStatus;
-@property BOOL hasGeofence;
+static NSString *text[2] = {
+    @"Tap to add geofence to your current location",
+    @"Tap to remove geofence",
+};
 
-@property NSInteger floorLevel;
-@property IACertainty floorCertainty;
+@interface AppleMapsOverlayViewController () <IALocationManagerDelegate, MKMapViewDelegate>
+@end
+
+@interface GeofenceViewController () <IALocationManagerDelegate>
+@property (nonatomic, strong) IAGeofence *geofence;
+@property (nonatomic, strong) MKCircle *overlay;
+@property (nonatomic, strong) UILabel *label;
+@property (nonatomic, strong) UITapGestureRecognizer *tap;
+@property (nonatomic, assign) bool inside;
 @end
 
 @implementation GeofenceViewController
 
-#pragma mark IALocationManagerDelegate methods
-/**
- * Position packet handling from IndoorAtlasPositioner
- */
-- (void)indoorLocationManager:(IALocationManager*)manager didUpdateLocations:(NSArray*)locations
-{
-    (void)manager;
-    IALocation *ialoc = [locations lastObject];
-    CLLocation *l = [ialoc location];
-    
-    // The accuracy of coordinate position depends on the placement of floor plan image.
-    NSLog(@"position changed to coordinate (lat,lon): %f, %f", l.coordinate.latitude, l.coordinate.longitude);
-    
-    // Register a new geofence once the position converges
-    if (l.horizontalAccuracy < 10 && self.hasGeofence==NO) {
-        
-        const double lat_per_meter = 9e-06*cos(M_PI/180.0*l.coordinate.latitude);
-        const double lon_per_meter = 9e-06;
-        
-        // Add a circular geofence by adding points with a 10 m radius clockwise
-        NSMutableArray<NSNumber*> *edges = [NSMutableArray array];
-        for (int i = 1; i <= 10; i++) {
-            double lat = l.coordinate.latitude + 10*lat_per_meter*sin(-2*M_PI*i/10);
-            double lon = l.coordinate.longitude + 10*lon_per_meter*cos(-2*M_PI*i/10);
-            [edges addObject:[NSNumber numberWithDouble:lat]];
-            [edges addObject:[NSNumber numberWithDouble:lon]];
-        }
-        IAGeofence *geo = [IAPolygonGeofence polygonGeofenceWithIdentifier:@"My geofence" andFloor:ialoc.floor edges:edges];
-        
-        [manager startMonitoringForGeofence:geo];
-        
-        self.hasGeofence = YES;
-        self.geofenceStatus = @"Registered geofence with 10 m radius.";
-        NSLog(@"New geofence registered.");
-        
+- (MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay {
+    if (overlay == self.overlay) {
+        MKCircleRenderer *circleRenderer = [[MKCircleRenderer alloc] initWithCircle:overlay];
+        circleRenderer.fillColor = [UIColor colorWithRed:!self.inside green:self.inside blue:0 alpha:1.0];
+        return circleRenderer;
     }
     
-    [self updateLabel];
+    return [super mapView:mapView rendererForOverlay:overlay];
 }
+
+#pragma mark IALocationManagerDelegate methods
 
 - (void)indoorLocationManager:(IALocationManager *)manager didEnterRegion:(IARegion *)region
 {
+    [super indoorLocationManager:manager didEnterRegion:region];
+    
     if (region.type == kIARegionTypeGeofence) {
-        self.geofenceStatus = @"Inside my geofence";
-        NSLog(@"Inside geofence.");
+        self.inside = true;
+        
+        if (self.overlay) {
+            [self.map removeOverlay:self.overlay];
+            [self.map addOverlay:self.overlay];
+        }
     }
-    [self updateLabel];
 }
 
 - (void)indoorLocationManager:(IALocationManager *)manager didExitRegion:(IARegion *)region
 {
     if (region.type == kIARegionTypeGeofence) {
-        self.geofenceStatus = @"Outside my geofence";
-        NSLog(@"Outside geofence.");
-    }
-    [self updateLabel];
-}
-
-- (void)updateLabel
-{
-    
-    NSString *status = @"Once the position estimate converges a geofence will be initialized to the current user location with a 10 m radius.";
-    
-    if (self.hasGeofence) {
-      status = @"Walking back into the geofence will trigger an Enter event it and walking out from it will trigger an Exit event.";
-    }
+        self.inside = false;
         
-    NSString * htmlString = [NSString stringWithFormat:@"<div style=\"text-align:center\"><big><h1>Geofence</h1><br>%@<br><hr><b>Status:</b><br>%@</div></big>",status,self.geofenceStatus];
-    
-    NSAttributedString * attrStr = [[NSAttributedString alloc] initWithData:[htmlString dataUsingEncoding:NSUnicodeStringEncoding] options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType } documentAttributes:nil error:nil];
-    
-    [_regionLabel setAttributedText:attrStr];
+        if (self.overlay) {
+            [self.map removeOverlay:self.overlay];
+            [self.map addOverlay:self.overlay];
+        }
+    }
 }
 
-#pragma mark IndoorAtlas API Usage
-
-/**
- * Authenticate to IndoorAtlas services and request location updates
- */
-- (void)requestLocation
+- (void)placeGeofence
 {
-    // Create IALocationManager and point delegate to receiver
-    self.manager = [IALocationManager sharedInstance];
-    self.manager.delegate = self;
+    IALocation *ialoc = self.locationManager.location;
+    CLLocation *clloc = ialoc.location;
     
-    // Optionally initial location
-    IALocation *location = [IALocation locationWithFloorPlanId:kFloorplanId];
-    self.manager.location = location;
+    const double lat_per_meter = 9e-06*cos(M_PI/180.0*clloc.coordinate.latitude);
+    const double lon_per_meter = 9e-06;
     
-    // Request location updates
-    [self.manager startUpdatingLocation];
+    // Add a circular geofence by adding points with a 5 m radius clockwise
+    NSMutableArray<NSNumber*> *edges = [NSMutableArray array];
+    for (int i = 1; i <= 10; i++) {
+        double lat = clloc.coordinate.latitude + 10*lat_per_meter*sin(-2*M_PI*i/10);
+        double lon = clloc.coordinate.longitude + 10*lon_per_meter*cos(-2*M_PI*i/10);
+        [edges addObject:[NSNumber numberWithDouble:lat]];
+        [edges addObject:[NSNumber numberWithDouble:lon]];
+    }
+    
+    if (self.geofence) {
+        [self.locationManager stopMonitoringForGeofence:self.geofence];
+        
+        [self.map removeOverlay:self.overlay];
+        self.overlay = nil;
+        self.geofence = nil;
+    } else {
+        self.overlay = [MKCircle circleWithCenterCoordinate:clloc.coordinate radius:5.0];
+        [self.map addOverlay:self.overlay];
+        
+        self.geofence = [IAPolygonGeofence polygonGeofenceWithIdentifier:@"My geofence" andFloor:ialoc.floor edges:edges];
+        
+        [self.locationManager startMonitoringForGeofence:self.geofence];
+    }
+    
+    self.label.text = text[(self.geofence != nil)];
 }
 
-#pragma mark boilerplate
-- (void)viewWillAppear:(BOOL)animated {
+- (void)viewWillAppear:(BOOL)animated
+{
     [super viewWillAppear:animated];
-    [self requestLocation];
-    
-    _regionLabel = [[UILabel alloc] initWithFrame:self.view.frame];
-    
-    [_regionLabel setTextColor:[UIColor blackColor]];
-    [_regionLabel setBackgroundColor:[UIColor whiteColor]];
-    [_regionLabel setFont:[UIFont fontWithName: @"Trebuchet MS" size: 14.0f]];
-    
-    [_regionLabel setNumberOfLines:0];
-    [self updateLabel];
-    
-    [self.view addSubview:_regionLabel];
-
-    self.geofenceStatus = @"Waiting for position to converge.";
-    [self updateLabel];
+    self.tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(placeGeofence)];
+    [self.view addGestureRecognizer:self.tap];
+    self.label = [UILabel new];
+    self.label.text = text[0];
+    self.label.textAlignment = NSTextAlignmentCenter;
+    self.label.numberOfLines = 0;
+    CGRect frame = self.view.bounds;
+    frame.origin.y = (frame.size.height = 24 * 2) / 2;
+    self.label.frame = frame;
+    [self.view addSubview:self.label];
 }
 
-- (void)viewWillDisappear:(BOOL)animated {
+- (void)viewWillDisappear:(BOOL)animated
+{
     [super viewWillDisappear:animated];
-    [self.manager stopUpdatingLocation];
-    self.manager.delegate = nil;
-    self.manager = nil;
+    [self.view removeGestureRecognizer:self.tap];
+    self.tap = nil;
+    [self.label removeFromSuperview];
+    self.label = nil;
+    [self.locationManager stopMonitoringForGeofence:self.geofence];
+    self.geofence = nil;
+    self.overlay = nil;
 }
 
 @end
