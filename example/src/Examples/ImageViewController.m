80176ab6f81b2ea157f2a7311bf6c6fd4b2bd5b3
Toni Nopanen <toni.nopanen@indooratlas.com>
2017-10-24 09:47:03 +0300

    Update ImageView example

--- example/src/Examples/ImageViewController.m
+++ example/src/Examples/ImageViewController.m
@@ -16,6 +16,7 @@
 @property (nonatomic, strong) IAFloorPlan *floorPlan;
 @property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic, strong) UIView *circle;
+@property (nonatomic, strong) UIView *accuracyCircle;
 @property (nonatomic, strong) IALocationManager *manager;
 @property (nonatomic, strong) IAResourceManager *resourceManager;
 @property (nonatomic, strong) CalibrationIndicator *calibrationIndicator;
@@ -41,9 +42,14 @@
         NSLog(@"position changed to pixel point: %fx%f", point.x, point.y);
         [UIView animateWithDuration:(self.circle.hidden ? 0.0f : 0.35f) animations:^{
             weakSelf.circle.center = point;
+            weakSelf.accuracyCircle.center = point;
+            CGFloat size = loc.location.horizontalAccuracy * [self.floorPlan meterToPixelConversion];
+            weakSelf.accuracyCircle.transform = CGAffineTransformMakeScale(size, size);
+            [self.view bringSubviewToFront:weakSelf.circle];
         }];
     }
 
+    self.accuracyCircle.hidden = NO;
     self.circle.hidden = NO;
     [self updateLabel];
 }
@@ -86,7 +92,7 @@
            return;
        }
 
-       NSLog(@"fetched floor plan with id: %@", floorplanId);
+       NSLog(@"Fetched floor plan with id: %@", floorplanId);
 
        imageFetch = [self.resourceManager fetchFloorPlanImageWithUrl:floorplan.imageUrl andCompletion:^(NSData *data, NSError *error) {
            if (error) {
@@ -126,12 +132,6 @@
     self.manager = [IALocationManager new];
     self.manager.delegate = self;
 
-    // Optionally set initial location
-    if (kFloorplanId.length) {
-        IALocation *location = [IALocation locationWithFloorPlanId:kFloorplanId];
-        self.manager.location = location;
-    }
-
     // Create floor plan manager
     self.resourceManager = [IAResourceManager resourceManagerWithLocationManager:self.manager];
 
@@ -146,20 +146,29 @@
 
 - (void)updateLabel
 {
-    self.label.text = [NSString stringWithFormat:@"Trace ID: %@", [self.manager.extraInfo objectForKey:kIATraceId]];
+    self.label.text = [NSString stringWithFormat:@"TraceID: %@", [self.manager.extraInfo objectForKey:kIATraceId]];
 }
 
 #pragma mark ImageViewContoller boilerplate
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    self.view.backgroundColor = [UIColor whiteColor];
     self.imageView = [UIImageView new];
     [self.view addSubview:self.imageView];
-
+    
+    self.accuracyCircle = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1, 1)];
+    self.accuracyCircle.backgroundColor = [UIColor colorWithRed:0.08627 green:0.5059 blue:0.9843 alpha:0.4];
+    self.accuracyCircle.layer.cornerRadius = self.accuracyCircle.frame.size.width / 2;
+    self.accuracyCircle.hidden = YES;
+    [self.imageView addSubview:self.accuracyCircle];
+    
     self.imageView.frame = self.view.frame;
     self.circle = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1, 1)];
     self.circle.backgroundColor = [UIColor colorWithRed:0.08627 green:0.5059 blue:0.9843 alpha:1.0];
     self.circle.layer.cornerRadius = self.circle.frame.size.width / 2;
+    self.circle.layer.borderColor = [[UIColor colorWithRed:1 green:1 blue:1 alpha:1] CGColor];
+    self.circle.layer.borderWidth = 0.1;
     self.circle.hidden = YES;
     [self.imageView addSubview:self.circle];
 
@@ -167,7 +176,7 @@
     self.label.textAlignment = NSTextAlignmentCenter;
     self.label.numberOfLines = 0;
     self.label.font = [UIFont fontWithName:@"Trebuchet MS" size:14.0f];
-    self.label.textColor = [UIColor whiteColor];
+    self.label.textColor = [UIColor blackColor];
     CGRect frame = self.view.bounds;
     frame.size.height = 24 * 2;
     self.label.frame = frame;
