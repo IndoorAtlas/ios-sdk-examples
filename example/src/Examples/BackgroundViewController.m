0961092ba89e8a1a6cba212400c735719fa628e4
Jari Vetoniemi <jari.vetoniemi@indooratlas.com>
2019-10-01 20:56:45 +0900

    Update the BG example
    Remove the weird heuristics and make it accurate as possible.

--- example/src/Examples/BackgroundViewController.m
+++ example/src/Examples/BackgroundViewController.m
@@ -7,44 +7,17 @@
 #import "BackgroundViewController.h"
 #import "../ApiKeys.h"
 
-// IndoorAtlas SDK will be started every locationUpdateInterval seconds to
-// request a location update
-const double locationUpdateInterval = 60.0;
-
-// if no IA location update is received withinlocationUpdateTimeout seconds,
-// IA location updates will be stopped to save battery
-// locationUpdateTimeout must be smaller than locationUpdateInterval
-const double locationUpdateTimeout = 10.0;
-
-// required accuracy for IndoorAtlas location updates. When this accuracy is achieved,
-// IA location updates will be stopped to save battery
-const double requiredAccuracy = 100.0; // meters
-
 @interface BackgroundViewController () <IALocationManagerDelegate, CLLocationManagerDelegate> {
     UILabel *label;
     NSString *floorPlanId;
     NSString *venueId;
 }
-@property (nonatomic, strong) IALocationManager *manager;
-@property (nonatomic, strong) CLLocationManager *clManager;
-@property (nonatomic, retain) NSTimer *silenceTimer;
-@property (nonatomic, retain) NSTimer *locationTimer;
+@property (nonatomic) IALocationManager *manager;
+@property (nonatomic) CLLocationManager *clManager;
+@property (nonatomic) NSDate *lastShow;
 @end
 
 @implementation BackgroundViewController
-
-+ (NSString *)backgroundTimeRemaining
-{
-    UIApplication *app = [UIApplication sharedApplication];
-    NSString *backgroundTime;
-    if (app.backgroundTimeRemaining == DBL_MAX) {
-        backgroundTime = @"unlimited";
-    } else {
-        backgroundTime = [@(app.backgroundTimeRemaining) stringValue];
-    }
-
-    return backgroundTime;
-}
 
 #pragma mark IALocationManagerDelegate methods
 /**
@@ -55,60 +28,21 @@ const double requiredAccuracy = 100.0; // meters
     (void)manager;
     CLLocation *l = [(IALocation*)locations.lastObject location];
 
-    [self.locationTimer invalidate];
-
     // The accuracy of coordinate position depends on the placement of floor plan image.
     NSString * timestamp = [NSString stringWithFormat:@"%f",[[NSDate date] timeIntervalSince1970] * 1000];
     NSLog(@"IA location received %@: (lat,lon, accuracy): %f, %f  accuracy: %lf", timestamp, l.coordinate.latitude, l.coordinate.longitude, l.horizontalAccuracy);
 
-    NSLog(@"background time remaining %@ seconds", [BackgroundViewController backgroundTimeRemaining]);
 
     // Showing notification is only to see that application is still running and not required for background mode
-    [self showNotification:[NSString stringWithFormat:@"Coord: %lf, %lf acc: %lf bg time remaining %@", l.coordinate.latitude, l.coordinate.longitude, l.horizontalAccuracy, [BackgroundViewController backgroundTimeRemaining]]];
-
-    // If location is accurate enough, stop location updates
-    if (l.horizontalAccuracy < requiredAccuracy) {
-        [self.manager stopUpdatingLocation];
-    }
+    [self showNotification:[NSString stringWithFormat:@"Coord: %lf, %lf acc: %lf", l.coordinate.latitude, l.coordinate.longitude, l.horizontalAccuracy]];
 }
 
 #pragma mark IndoorAtlas API Usage
 
-/**
- * Authenticate to IndoorAtlas services and request location updates
- */
-- (void)requestLocation
-{
-
-    // shuts down IALocationManager if no location is received within timeout seconds
-    self.locationTimer = [NSTimer scheduledTimerWithTimeInterval:locationUpdateTimeout target:self
-                                                       selector:@selector(timeoutRequestingLocation) userInfo:nil repeats:NO];
-    NSLog(@"TraceId %@", [self.manager.extraInfo objectForKey:kIATraceId]);
-}
-
-- (void)timeoutRequestingLocation
-{
-    NSLog(@"Timeout, no location update within %lf seconds.",  locationUpdateTimeout);
-    NSLog(@"Background time remaining %@ seconds", [BackgroundViewController backgroundTimeRemaining]);
-
-    [self.manager stopUpdatingLocation];
-}
-
 - (void)startBackgroundTask
 {
-    UIBackgroundTaskIdentifier bgTask;
-    UIApplication *app = [UIApplication sharedApplication];
-    bgTask = [app beginBackgroundTaskWithExpirationHandler:^{
-        NSLog(@"Background task ended");
-        [app endBackgroundTask:bgTask];
-    }];
-    self.silenceTimer = [NSTimer scheduledTimerWithTimeInterval:locationUpdateInterval target:self
-                                                       selector:@selector(requestLocation) userInfo:nil repeats:YES];
-
     // CLLocationManager is started called to keep background task alive
     [self.clManager startUpdatingLocation];
-
-    [self requestLocation];
 }
 
 #pragma mark CoreLocation Manager
@@ -147,8 +81,8 @@ const double requiredAccuracy = 100.0; // meters
 {
     self.clManager = [CLLocationManager new];
     self.clManager.delegate = self;
-    self.clManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers;
-    self.clManager.distanceFilter = 9999999;
+    self.clManager.desiredAccuracy = kCLLocationAccuracyBest;
+    self.clManager.distanceFilter = kCLDistanceFilterNone;
     self.clManager.pausesLocationUpdatesAutomatically = NO;
     [self authenticateCLLocationManager];
 }
@@ -162,6 +96,10 @@ const double requiredAccuracy = 100.0; // meters
 #pragma mark User interface
 - (void)showNotification:(NSString *)message
 {
+    if (self.lastShow && [self.lastShow timeIntervalSinceNow] < 1.0)
+        return;
+
+    self.lastShow = [NSDate date];
     UILocalNotification *notification = [[UILocalNotification alloc]init];
     [notification setAlertBody:message];
     [notification setFireDate:[NSDate dateWithTimeIntervalSinceNow:1]];
@@ -172,7 +110,7 @@ const double requiredAccuracy = 100.0; // meters
 
 - (void)updateLabel
 {
-    NSString *htmlString = [NSString stringWithFormat:@"<div style=\"text-align:center\"><big><b>Background Example</b><br>If application is backgrounded while this view is showing, this example keeps running periodically on the background and every %.1lf seconds uses IndoorAtlas SDK to get location information. Increases battery usage.<br/><br/><b>Trace ID</b><br/>%@</big></div>", locationUpdateInterval,  [self.manager.extraInfo objectForKey:kIATraceId]];
+    NSString *htmlString = [NSString stringWithFormat:@"<div style=\"text-align:center\"><big><b>Background Example</b><br/><br/><b>Trace ID</b><br/>%@</big></div>", [self.manager.extraInfo objectForKey:kIATraceId]];
 
     NSAttributedString *attrStr = [[NSAttributedString alloc] initWithData:[htmlString dataUsingEncoding:NSUnicodeStringEncoding] options:@{ NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType } documentAttributes:nil error:nil];
 
@@ -182,15 +120,14 @@ const double requiredAccuracy = 100.0; // meters
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     self.view.backgroundColor = [UIColor whiteColor];
-    
+
     // Create IALocationManager and point delegate to receiver
     self.manager = [IALocationManager sharedInstance];
     self.manager.delegate = self;
-    self.manager.desiredAccuracy = kIALocationAccuracyLow;
-    
+
     // Request location updates
     [self.manager startUpdatingLocation];
-    
+
     [self setupCLLocationManager];
     [self startBackgroundTask];
 
